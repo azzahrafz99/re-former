afe961dfbb489cd90ac052bbe5a98f1f443fade2
Fatimah Az-Zahra <azzahrafz@gmail.com>
2017-06-05 14:01:03 +0700

    finishing form project

---
+++ db/migrate/20170605045006_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username, unique: true, :nill => false
+      t.string :email,    unique: true, :nill => false
+      t.string :password,               :nill => false
+      t.timestamps
+    end
+  end
+end
